0f6954b01b17cef88b325326c84edb9a5f744e1a
Viacheslav Voytovich <voytovich.slava@gmail.com>
2014-02-23 14:06:24 +0800

    Some changes

--- build.js
+++ build.js
@@ -1,27 +1,20 @@
+
 var bundle = require('browserify')(),
-    fs = require('fs');
+    fs = require('fs'),
     uglify = require('uglify-js');
 
-function buildCompress (source) {
-    fs.writeFile('./src/DataBind.min.js', uglify.minify(source, {fromString: true}).code, function (err) {
+function buildCompress (source, filePath) {
+    fs.writeFile(filePath, uglify.minify(source, {fromString: true}).code, function (err) {
         if (err) throw err;
     });
 }
 
-function buildSimple (source) {
-    fs.writeFile('./src/DataBind.min.js', source, function (err) {
-        if (err) throw err;
-    });
-}
-
-function build (source) {
-    buildCompress(source);
-    //buildSimple(source);
+function build (source, filePath) {
+    buildCompress(source, filePath);
 }
 
 bundle.add('./src/DataBind.js');
 bundle.bundle({standalone: 'DataBind'}, function (err, source) {
     if (err) console.error(err);
-    fs.writeFileSync('DataBind.bundle.js', source);
-    build(source);
+    build(source, './src/DataBind.min.js');
 });
